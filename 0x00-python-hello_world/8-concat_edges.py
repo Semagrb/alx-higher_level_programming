05d0df02385259dd9d36d1782e4016b5573f8b36
semagrb <fithisalma@gmail.com>
2023-10-03 17:29:24 -0700

    task 8 has been updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str_value = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str_result = str_value[39:66] + str_value[107:112] + str_value[:6]
+str_result = str_value[39:66] + str_value[113:119] + str_value[:6]
 print(str_result)
