7430b6cbf2cb7658582359c40380b236a898122f
Semagrb <fithisalma@gmail.com>
2023-10-11 18:04:30 -0700

    updated

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,18 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
+    if a_dictionary:
+        max_key = None
+        max_value = float('-inf')
+        for key, value in a_dictionary.items():
+            if value > max_value:
+                max_key = key
+                max_value = value
+        return max_key
+    return None
 
-    return (max(a_dictionary, key=a_dictionary.get))
+# Test cases
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
