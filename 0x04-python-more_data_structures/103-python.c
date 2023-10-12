79073993f5a8d97d7c562a2ee508192e1a1618ba
Semagrb <fithisalma@gmail.com>
2023-10-11 18:40:37 -0700

    tasks

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,44 +4,42 @@ import struct
 import ctypes
 from types import ListType
 
-def print_python_bytes(p):
-    if not isinstance(p, bytes):
-        print("[.] bytes object info")
-        print("  [ERROR] Invalid Bytes Object")
-        return
+def print_python_bytes(p) :
+	if not isinstance(p, bytes) :
+		print("[.] bytes object info")
+		print("  [ERROR] Invalid Bytes Object")
+		return
 
-    print("[.] bytes object info")
-    size = len(p)
-    print("  size: {}".format(size))
-    print("  trying string: {}".format(p.decode("utf-8", "ignore").replace('\x00', '')))
-    limit = min(size, 10)
-    print("  first {} bytes:".format(limit))
+	print("[.] bytes object info")
+size = len(p)
+	print("  size: {}".format(size))
+	print("  trying string: {}".format(p.decode("utf-8", "ignore").replace('\x00', '')))
+limit = min(size, 10)
+	print("  first {} bytes:".format(limit))
 
-    for i in range(limit):
-        byte = struct.unpack('B', p[i:i+1])[0]
-        print(" {:02x}".format(byte), end="")
-    print()
+	for i in range(limit) :
+		byte = struct.unpack('B', p[i : i + 1])[0]
+		print(" {:02x}".format(byte), end = "")
+		print()
 
+	def print_python_list(p) :
+		if type(p) is not ListType :
+			return
 
-def print_python_list(p):
-    if type(p) is not ListType:
-        return
+	print("[*] Python list info")
+size = len(p)
+	print("[*] Size of the Python List = {}".format(size))
+	allocated = getattr(p, 'allocated', 0)
+	print("[*] Allocated = {}".format(allocated))
 
-    print("[*] Python list info")
-    size = len(p)
-    print("[*] Size of the Python List = {}".format(size))
-    allocated = getattr(p, 'allocated', 0)
-    print("[*] Allocated = {}".format(allocated))
+	for i, item in enumerate(p) :
+		obj_type = item.__class__.__name__
+		print("Element {}: {}".format(i, obj_type))
+		if isinstance(item, bytes) :
+			print_python_bytes(item)
 
-    for i, item in enumerate(p):
-        obj_type = item.__class__.__name__
-        print("Element {}: {}".format(i, obj_type))
-        if isinstance(item, bytes):
-            print_python_bytes(item)
-
-# Example usage:
-my_bytes = b'Hello, World!'
-my_list = [b'Python', b'Programming', b'is', b'fun']
-
-print_python_bytes(my_bytes)
+# Example usage :
+			my_bytes = b'Hello, World!'
+			my_list = (b'Python', b'Programming', b'is', b'fun')
+	print_python_bytes(my_bytes)
 print_python_list(my_list)
