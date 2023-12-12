3b7a3c3bb92f6243e26ee024f9bc4ab77bde0a4f
semagrb <fithisalma@gmail.com>
2023-12-12 14:05:57 -0800

    tasks

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,9 @@
+-- 12-no_cheating.sql
+
+USE hbtn_0c_0;
+
+-- Update the score of Bob to 10 using the name field
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,8 @@
+-- 13-change_class.sql
+
+USE hbtn_0c_0;
+
+-- Delete records with a score <= 5 from second_table
+DELETE FROM second_table
+WHERE score <= 5;
+
