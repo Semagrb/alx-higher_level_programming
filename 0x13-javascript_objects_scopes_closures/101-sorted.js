034fce0801443e55c8d822e215833eb76749f342
semagrb <fithisalma@gmail.com>
2024-01-09 15:45:12 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-const { dict } = require('./101-data');
+const dict = require('./101-data').dict;
 
 const totalist = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const val of valsUniq) {
- const list = totalist.filter(([key, value]) => value === val).map(([key]) => key);
- newDict[val] = list;
-}
+
+valsUniq.forEach(value => {
+ const list = totalist.filter(entry => entry[1] === value).map(entry => entry[0]);
+ newDict[value] = list;
+});
+
 console.log(newDict);
