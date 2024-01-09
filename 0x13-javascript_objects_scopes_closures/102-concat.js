e4713e55132183582255c78b95919aed81a046ba
semagrb <fithisalma@gmail.com>
2024-01-09 15:48:11 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,32 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs').promises;
+const fs = require('fs');
 
-const fArg = process.argv[2];
-const sArg = process.argv[3];
-const dArg = process.argv[4];
-
-const readFile = async (file) => {
- try {
-    const data = await fs.readFile(file, 'utf8');
-    return data;
- } catch (err) {
-    console.error(err);
- }
-};
-
-const writeFile = async (data, file) => {
- try {
-    await fs.writeFile(file, data);
- } catch (err) {
-    console.error(err);
- }
-};
-
-const concatFiles = async () => {
- const fArgData = await readFile(fArg);
- const sArgData = await readFile(sArg);
- const combinedData = fArgData + sArgData;
- await writeFile(combinedData, dArg);
-};
-
-concatFiles();
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
