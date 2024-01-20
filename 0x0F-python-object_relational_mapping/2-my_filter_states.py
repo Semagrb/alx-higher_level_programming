00ed7d1986d87d397ce5a058d9dee4b74dfef87b
semagrb <fithisalma@gmail.com>
2024-01-19 16:15:41 -0800

    Fixed error task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Module that lists all states from the h_0e_0_usa database.
-"""
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-
-    # Get MySQL credentials and search name from command-line arguments
-    db = MySQLdb.connect(
-        user=sys.argv[1],  # MySQL user
-        passwd=sys.argv[2],  # MySQL password
-        db=sys.argv[3]  # Database name
-    )
-
-    # Create a cursor object to interact with the database
-    c = db.cursor()
-
-    # Define the SQL query to retrieve states with the specified name
-    query = """
-        SELECT *
-        FROM `states`
-        WHERE BINARY `name` = %s
-    """
-
-    # Execute the SQL query with the specified name as a parameter
-    c.execute(query, (sys.argv[4],))
-
-    # Fetch all rows and print the states
-    [print(state) for state in c.fetchall()]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
