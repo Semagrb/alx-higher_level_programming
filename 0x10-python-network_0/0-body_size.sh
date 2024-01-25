748f33e8add348467908bf00e7777f44c2d5ab61
semagrb <fithisalma@gmail.com>
2024-01-25 11:18:15 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]
-then
- echo "No URL provided. Please run the script with a URL as an argument."
- exit 1
-fi
-
-# Send a GET request to the URL and store the response in a variable
-response=$(curl -s $1)
-
-# Display the size of the body of the response in bytes
-echo "Size of the body of the response (in bytes): $(echo ${#response}))"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
