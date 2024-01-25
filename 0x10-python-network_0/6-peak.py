dbf9c9467116ed592524cbf0cc51f2421a9bef66
semagrb <fithisalma@gmail.com>
2024-01-25 12:03:40 -0800

    error fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,27 @@
 
 def find_peak(list_of_integers):
 
-    if not list_of_integers:
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
-    mid_idx = len(list_of_integers) // 2
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    if mid_idx > 0 and mid_idx < len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] > list_of_integers[mid_idx + 1]:
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
             return list_of_integers[mid_idx]
-
-    if mid_idx == 0 or list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-        a_list = list_of_integers[mid_idx + 1:]
     else:
-        a_list = list_of_integers[:mid_idx]
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
 
-    return find_peak(a_list)
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)	
