8c12ebebd5051069caf9f619148f13a90d9e290b
semagrb <fithisalma@gmail.com>
2024-01-25 11:16:09 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Check if URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
+# Check if a URL was provided as an argument
+if [ -z "$1" ]
+then
+ echo "No URL provided. Please run the script with a URL as an argument."
+ exit 1
 fi
 
-# Get the URL from command line arguments
-url=$1
+# Send a GET request to the URL and store the response in a variable
+response=$(curl -s $1)
 
-# Use curl to send a request and display the size of the response body in bytes
-size=$(curl -sI $url | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
-
-echo $size
+# Display the size of the body of the response in bytes
+echo "Size of the body of the response (in bytes): $(echo ${#response}))"
