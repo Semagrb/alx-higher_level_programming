0c669a2be9ebcfecc6a0b4307146fc605cf3d946
semagrb <fithisalma@gmail.com>
2024-01-25 11:59:23 -0800

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
+
 def find_peak(list_of_integers):
-    if not list_of_integers or len(list_of_integers) == 0:
+
+    if not list_of_integers:
         return None
 
     mid_idx = len(list_of_integers) // 2
 
-    if (mid_idx != len(list_of_integers) - 1 and
-            list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and
-            list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]) or \
-       (mid_idx == len(list_of_integers) - 1 and
-            list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]):
-        return list_of_integers[mid_idx]
+    if mid_idx > 0 and mid_idx < len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] > list_of_integers[mid_idx + 1]:
+            return list_of_integers[mid_idx]
 
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        return find_peak(list_of_integers[:mid_idx])
+    if mid_idx == 0 or list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+        a_list = list_of_integers[mid_idx + 1:]
     else:
-        return find_peak(list_of_integers[mid_idx + 1:])
+        a_list = list_of_integers[:mid_idx]
+
+    return find_peak(a_list)
