ac4866dfe0c898d9a9670f6a4a71dc38f62b45ee
semagrb <fithisalma@gmail.com>
2024-01-25 10:52:41 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
+# Take in a URL as a command line argument
+url=$1
+
+# If no URL is provided, print a usage message and exit the script
+if [ -z "$url" ]; then
+ echo "Usage: $0 <URL>"
+ exit 1
 fi
 
-# Send a request to the URL and display the size of the response body in bytes
-curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f2
+# Send a GET request to the specified URL with the '-s' flag to make the request silent
+# and the '-I' flag to make a HEAD request instead of a GET request.
+# This means we're only retrieving the headers of the response, not the body.
+curl_response=$(curl -sI "$url")
+
+# Check if the 'Content-Length' header is present in the response.
+# If it is, the 'grep' command will return a non-empty string.
+if echo "$curl_response" | grep -q "Content-Length"; then
+ # If the 'Content-Length' header is present, extract its value using 'cut'.
+ # The '-d " "' flag tells 'cut' to use spaces as the delimiter,
+ # the '-f2' flag tells 'cut' to extract the second field,
+ # which is the value of the 'Content-Length' header.
+ content_length=$(echo "$curl_response" | grep "Content-Length" | cut -d " " -f2)
+
+ # If the 'Content-Length' header is not present, print an error message.
+else
+ echo "Error: The 'Content-Length' header is not present in the response."
+ exit 1
+fi
+
+# Finally, print the size of the body of the response, which is the value of the 'Content-Length' header.
+echo "The size of the body of the response is $content_length bytes."
