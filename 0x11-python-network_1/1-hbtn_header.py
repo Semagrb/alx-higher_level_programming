5d79d4dce4449b3a0e36470e1ec23b0d3fea7075
semagrb <fithisalma@gmail.com>
2024-01-26 13:39:39 -0800

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-Python script that sends a request to a specified URL,
-fetches the response, and displays the value of the
-X-Request-Id variable found in the response header.
+Displays the X-Request-Id header variable of a GET request to a specified URL.
+
+Usage: ./1-hbtn_header.py <URL>
 """
 
-from urllib import request
-from sys import argv
+import sys
+import urllib.request
 
 if __name__ == "__main__":
     # Check if a URL is provided as a command-line argument
-    if len(argv) != 2:
-        print("Usage: ./script.py <URL>")
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
     else:
         # Retrieve the URL from the command line
-        url = argv[1]
+        url = sys.argv[1]
 
-        # Send a request to the specified URL
-        with request.urlopen(url) as response:
-            # Retrieve and display the value of the X-Request-Id variable from the header
-            x_request_id = response.getheader("X-Request-Id")
-            print(f"Value of X-Request-Id: {x_request_id}"
+        # Create a GET request for the specified URL
+        request = urllib.request.Request(url)
+
+        # Open the URL and retrieve the response
+        with urllib.request.urlopen(request) as response:
+            # Extract the X-Request-Id header variable and display its value
+            x_request_id = dict(response.headers).get("X-Request-Id")
+            print(f"X-Request-Id: {x_request_id}")
