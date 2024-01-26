e3426666635c8b50be77c2c03b7587c0564a11af
semagrb <fithisalma@gmail.com>
2024-01-26 13:17:26 -0800

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that takes a URL as input, sends a GET request to the specified URL,
-and displays the value of the X-Request-Id variable found in the response header.
-"""
+"""Python script that takes in a URL, sends
+a request to the URL and displays the value of
+the variable X-Request-Id in the response header."""
 
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    # Check if a URL is provided as a command-line argument
-    if len(argv) != 2:
-        print("Usage: ./script.py <URL>")
-    else:
-        # Retrieve the URL from the command line
-        url = argv[1]
-
-        # Send a GET request to the specified URL using the requests library
-        response = requests.get(url)
-
-        # Retrieve and display the value of the X-Request-Id variable from the response header
-        x_request_id = response.headers.get("X-Request-Id")
-        print(f"Value of X-Request-Id: {x_request_id}")
+    print(requests.get(argv[1]).headers.get("X-Request-Id"))
