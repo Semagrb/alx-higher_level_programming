57433fe88d7c7432c9212e57abe391ef48685bba
semagrb <fithisalma@gmail.com>
2024-02-20 16:31:30 -0800

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,16 +7,20 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else if (response.statusCode === 200) {
+    const completedTasks = {};
     const tasks = JSON.parse(body);
-    let completedTasksCount = 0;
-
+    
     for (const task of tasks) {
-      if (task.completed) {
-        completedTasksCount++;
+      if (task.completed === true) {
+        if (completedTasks[task.userId] === undefined) {
+          completedTasks[task.userId] = 1;
+        } else {
+          completedTasks[task.userId]++;
+        }
       }
     }
-
-    console.log(completedTasksCount);
+    
+    console.log(completedTasks);
   } else {
     console.error('An error occurred. Status code: ' + response.statusCode);
   }
